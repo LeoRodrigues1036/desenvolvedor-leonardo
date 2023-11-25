b3eaacb6c307373d8cd323858046a5f8e03d9a27
Leonardo Rodrigues <leorodrigues1036@gmail.com>
2023-11-25 14:15:36 -0300

    deploy arquivos

---
+++ serve.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+// Configura o middleware express.static para servir arquivos estáticos da pasta 'public'
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// Define rotas para diferentes arquivos HTML
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/sobre', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'sobre.html'));
+});
+
+app.get('/portfolio', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'portfolio.html'));
+});
+
+app.get('/contato', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'contato.html'));
+});
+
+app.listen(port, () => {
+  console.log(`O servidor está rodando em http://localhost:${port}`);
+});
+
